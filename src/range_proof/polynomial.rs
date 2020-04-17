f1d6be8dcc58012ba37a8e23501e2d549f519cc8
Rohit Narurkar <rohitnarurkar@gmail.com>
2020-04-18 05:13:14 +0530

    compute w3

--- src/range_proof/polynomial.rs
+++ src/range_proof/polynomial.rs
@@ -39,7 +39,7 @@ pub fn compute_w1_w2(
     z: &Fr
 ) -> (Polynomial<Fr>, Polynomial<Fr>) {
     let one = Fr::one();
-    let w_n_minus_1 = domain.elements().last().unwrap() / domain.group_gen;
+    let w_n_minus_1 = domain.elements().last().unwrap();
 
     // polynomial: P(x) = x - w^(n-1)
     let x_minus_w_n_minus_1 =
@@ -67,6 +67,44 @@ pub fn compute_w1_w2(
     let w2: Polynomial::<Fr> = &(&(g * &one_minus_g) * &x_n_minus_1) / &x_minus_w_n_minus_1;
 
     (w1, w2)
+}
+
+pub fn compute_w3(
+    domain: &EvaluationDomain<Fr>,
+    g: &Polynomial<Fr>
+) -> Polynomial<Fr> {
+    // w3: [g(X) - 2g(Xw)] * [1 - g(X) + 2g(Xw)] * [X - w^(n-1)]
+    // degree of g = n - 1
+    // degree of w3 = (n - 1) + (n - 1) + 1 = 2n - 1
+    // the new domain can be of size 2n
+    let domain_2n = EvaluationDomain::<Fr>::new(2 * domain.size()).unwrap();
+
+    // find evaluations of g in the new domain
+    let mut g_evals = domain_2n.fft(&g);
+
+    // since we have doubled the domain size
+    // the roots of unity of the new domain will also occur
+    // in between the roots of unity of the original domain.
+    // hence, if g(X) <- g_evals[i]
+    // then g(Xw) <- g_evals[i+2]
+    g_evals.push(g_evals[0]);
+    g_evals.push(g_evals[1]);
+
+    // calculate evaluations of w3
+    let w_n_minus_1 = domain.elements().last().unwrap();
+    let two = Fr::from(2u8);
+    let w3_evals: Vec<Fr> = (0..domain_2n.size())
+        .zip(domain_2n.elements())
+        .into_iter()
+        .map(|(i, x_i)| {
+            let part_a = g_evals[i] - (two * g_evals[i + 2]);
+            let part_b = Fr::one() - g_evals[i] + (two * g_evals[i + 2]);
+            let part_c = x_i - w_n_minus_1;
+            part_a * part_b * part_c
+        })
+        .collect();
+
+    Polynomial::<Fr>::from_coefficients_vec(domain_2n.ifft(&w3_evals))
 }
 
 #[cfg(test)]
@@ -139,10 +177,47 @@ mod test {
         let part_a = g.evaluate(r);
         let part_b = Fr::one() - part_a;
         let part_c = {
-            let w_n_minus_1 = domain.elements().last().unwrap() / domain.group_gen;
+            let w_n_minus_1 = domain.elements().last().unwrap();
             (r.pow(&n_as_ref) - Fr::one()) / (r - w_n_minus_1)
         };
         let w2_expected = part_a * part_b * part_c;
         assert_eq!(w2.evaluate(r), w2_expected);
+    }
+
+    #[test]
+    fn test_compute_w3() {
+        let n = 8usize;
+        let domain: EvaluationDomain<Fr> = EvaluationDomain::<Fr>::new(n).unwrap();
+        let z = Fr::from(83u8);
+        let g = compute_g(&domain, &z);
+
+        let w3 = compute_w3(&domain, &g);
+
+        // w3 should evaluate to 0 at all roots of unity for original domain
+        for root in domain.elements() {
+            assert!(w3.evaluate(root).is_zero());
+        }
+
+        // w3 degree should be 2n - 1
+        assert_eq!(w3.degree(), 2 * domain.size() - 1);
+
+        // evaluate w3 at a random field element
+        let w_n_minus_1 = domain.elements().last().unwrap();
+        let mut rng = rand::thread_rng();
+        let r = Fr::from(rng.gen::<u64>());
+        let part_a = g.evaluate(r) - (Fr::from(2u8) * g.evaluate(r * domain.group_gen));
+        let part_b = Fr::one() - g.evaluate(r) + (Fr::from(2u8) * g.evaluate(r * domain.group_gen));
+        let part_c = r - w_n_minus_1;
+        let w3_expected = part_a * part_b * part_c;
+        assert_eq!(w3.evaluate(r), w3_expected);
+
+        // evaluate w3 at another random field element
+        let mut rng = rand::thread_rng();
+        let r = Fr::from(rng.gen::<u64>());
+        let part_a = g.evaluate(r) - (Fr::from(2u8) * g.evaluate(r * domain.group_gen));
+        let part_b = Fr::one() - g.evaluate(r) + (Fr::from(2u8) * g.evaluate(r * domain.group_gen));
+        let part_c = r - w_n_minus_1;
+        let w3_expected = part_a * part_b * part_c;
+        assert_eq!(w3.evaluate(r), w3_expected);
     }
 }
